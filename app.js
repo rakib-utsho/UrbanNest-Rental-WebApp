2770c45590dd1d9a63acebf3d9ad2bd5c2e8f5d0
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-26 18:08:25 +0600

    listing model

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// Require Express
 const express = require("express");
 const app = express();
-// Require Express
-const mongoose = require("mongoose");
 // Require Mongoose
+const mongoose = require("mongoose");
+// Require Model
+const Listing = require("./models/listing.js");
+
 
 // Connect MongoDB Database
+const MONGO_URL = "mongodb://127.0.0.1:27017/urbannest";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+// Call main function
 main()
 .then(()=>{
     console.log("Connected to DB successful");
@@ -12,16 +21,25 @@ main()
     console.log(err);
 });
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/urbannest";
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
 
 // REST API Creation
 // Root Route
 app.get("/", (req, res)=>{
     res.send("Root Route");
-})
+});
+// test route
+app.get("/testlisting", async (req,res)=>{
+    let sampleListing = new Listing({
+        title: "My Cottage",
+        description: "By the beach",
+        price: 12000,
+        location: "Maldive Islands",
+        country:"Maldives"
+    });
+    await sampleListing.save()
+    console.log("sample was saved");
+    res.send("successful testing");
+});
 
 app.listen(8080, ()=>{
     console.log("Server is Listening on port 8080");
---
+++ models/listing.js
@@ -0,0 +1,27 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema 
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: { 
+        type: String,
+        default: "https://images.unsplash.com/photo-1602002418816-5c0aeef426aa?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v)=> v ==="" ? "https://images.unsplash.com/photo-1602002418816-5c0aeef426aa?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+// Create Listing Model
+const Listing = mongoose.model("Listing", listingSchema);
+
+// Export Model
+module.exports = Listing;
+
