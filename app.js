6d9b4a8977a4fc2a521790fcd646310169ca8291
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-26 14:15:02 +0600

    basic setup

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ const app = express();
 const mongoose = require("mongoose");
 // Require Mongoose
 
+// Connect MongoDB Database
+main()
+.then(()=>{
+    console.log("Connected to DB successful");
+}).catch((err)=>{
+    console.log(err);
+});
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/urbannest";
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+// REST API Creation
+// Root Route
+app.get("/", (req, res)=>{
+    res.send("Root Route");
+})
+
 app.listen(8080, ()=>{
     console.log("Server is Listening on port 8080");
 });
-// Configure Local server port
+// Configure Localhost server port
