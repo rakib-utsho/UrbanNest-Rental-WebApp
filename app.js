deee4377d518fdcc7b5fd9e59e2e4ddde438c80f
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-26 22:32:25 +0600

    index route

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const app = express();
 const mongoose = require("mongoose");
 // Require Model
 const Listing = require("./models/listing.js");
+// Require Path
+const path = require("path");
 
+
+// set path and others uses
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Connect MongoDB Database
 const MONGO_URL = "mongodb://127.0.0.1:27017/urbannest";
@@ -27,19 +33,26 @@ main()
 app.get("/", (req, res)=>{
     res.send("Root Route");
 });
-// test route
-app.get("/testlisting", async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "My Cottage",
-        description: "By the beach",
-        price: 12000,
-        location: "Maldive Islands",
-        country:"Maldives"
-    });
-    await sampleListing.save()
-    console.log("sample was saved");
-    res.send("successful testing");
+
+// index route
+app.get("/listings", async (req, res)=> {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
+// test route
+// app.get("/testlisting", async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My Cottage",
+//         description: "By the beach",
+//         price: 12000,
+//         location: "Maldive Islands",
+//         country:"Maldives"
+//     });
+//     await sampleListing.save()
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
+ 
 
 app.listen(8080, ()=>{
     console.log("Server is Listening on port 8080");
