d373f8461335b1a6824a99b10e3363a776fac8f2
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-26 19:07:58 +0600

    initialize database

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+// Connect MongoDB Database
+const MONGO_URL = "mongodb://127.0.0.1:27017/urbannest";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+// Call main function
+main()
+.then(()=>{
+    console.log("Connected to DB successful");
+}).catch((err)=>{
+    console.log(err);
+});
+
+const initDB = async () =>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized")
+}
+
+initDB();
--- models/listing.js
+++ models/listing.js
@@ -5,19 +5,25 @@ const Schema = mongoose.Schema;
 //Create Schema 
 const listingSchema = new Schema({
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: String,
-    image: { 
-        type: String,
-        default: "https://images.unsplash.com/photo-1602002418816-5c0aeef426aa?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v)=> v ==="" ? "https://images.unsplash.com/photo-1602002418816-5c0aeef426aa?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        set: (v) =>
+            v === "" 
+                ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" // If an empty string, return the default link
+                : typeof v === "object" && v !== null && v.url 
+                ? v.url // If an object with `url`, return the `url`
+                : v, // Otherwise, return the value as-is
     },
     price: Number,
     location: String,
-    country: String
-});
+    country: String,
+  });
 
 // Create Listing Model
 const Listing = mongoose.model("Listing", listingSchema);
